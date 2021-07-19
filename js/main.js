a1f18ff0c907bc6b4e710e912257bc88a87f96b0
CassioD <cassiodans@gmail.com>
2021-07-19 00:30:08 -0300

    Primeira aula de java finalizada

---
+++ js/main.js
@@ -0,0 +1,79 @@
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+}
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+
+/*function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+};
+*/
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+alert(soma(5, 10));
+*/
+/*
+var d = new Date();
+alert (d.getDay());
+*/
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;
+};
+*/
+
+/*
+var idade = prompt("Qual é sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+
+/*var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.join(" - "));
+
+
+//var nome="Rafael Galleani";
+//var n1 = 29;
+//var n2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+
+//console.log(nome);
+//console.log(n1 + n2);
+//console.log(frase.replace("Japão","Brasil"));
+
+//alert(frase.replace("Japão","Brasil"));
